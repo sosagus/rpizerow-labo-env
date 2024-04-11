506661aa54a7d620446bb7727d084c8821789233
sosagus <164385194+sosagus@users.noreply.github.com>
2024-04-10 23:00:58 -0300

    chore: comentario del código añadido

--- actividades_python/ej_01/main.py
+++ actividades_python/ej_01/main.py
@@ -1,9 +1,12 @@
+## Importo los paquetes de código de las librerías "gpiozero" y "signal"
 from gpiozero import LED, Button
 from signal import pause
 
+## Asigno al LED y al pulsador sus respectivos pines
 led = LED (19)
 button = Button(18)
 
+## Ejecuto el comando para que, cuando presione el pulsador, el LED se encienda, y cuando no lo haga este se apague.
 button.when_pressed = led.on
 button.when_released = led.off
 
